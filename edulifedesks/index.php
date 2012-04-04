7e9399b5688cd77788835dab85b8650569a939aa
Lisa Walley <lwalley@eol.org>
2012-04-04 12:15:19 -0400

    Initializing LifeDesks repository for static maintenance files.

---
+++ edulifedesks/index.php
@@ -0,0 +1,36 @@
+<?php
+header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
+header('Status: 503 Service Temporarily Unavailable');
+header('Retry-After: 86400');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="noindex, nofollow" />
+  <title>EduLifeDesks is offline for maintenance</title>
+  <link href="css/learning.css" type="text/css" media="all" rel="stylesheet"/>
+</head>
+<body>
+  <div id="page"><div id="page-inner">
+    <div id="header">
+      <div id="logo-title">
+        <img id="logo" alt="Encyclopedia of Life logo" src="images/logo.gif" />
+        <h1>Education LifeDesks</h1>
+        <div id="site-slogan">Encyclopedia of Life tools for educators, students and enthusiasts worldwide.</div>
+      </div>
+    </div>
+    <div id="main"><div id="main-inner">
+      <div id="content"><div id="content-inner">
+        <h2>Under Maintenance</h2>
+        <p>We're sorry, Education LifeDesks are currently undergoing routine maintenance. We will be back online shortly.</p>
+        <p>Education LifeDesks are brought to you by the <a href="http://www.eol.org">Encyclopedia of Life (EOL)</a>, for more information please visit the <a href="http://education.eol.org">Learning + Education Group</a>.</p>
+      </div></div>
+    </div></div>
+    <div id="footer">
+      <p>Encyclopedia of Life Learning + Education Group | Museum of Comparative Zoology | Harvard University 26 Oxford Street, Cambridge, MA 02318 USA | 617-496-6764 | education [at] eol.org</p>
+    </div>
+  </div></div>
+</body>
+</html>
---
+++ lifedesks/index.php
@@ -0,0 +1,48 @@
+<?php
+header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
+header('Status: 503 Service Temporarily Unavailable');
+header('Retry-After: 86400');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="noindex, nofollow" />
+<title>Maintenance | LifeDesks</title>
+<link href="css/screen.css" rel="stylesheet" type="text/css" />
+</head>
+<body style="padding-top: 0 important!;">
+
+<div id="wrapper">
+	<div id="container">
+
+	<!-- header -->
+	<div id="header">
+		<div id="logo"><a href="/"><h1 class="site-title">LifeDesks</h1><img src="images/logo.png" alt="" /></a></div>
+	</div>
+	<!-- /header -->
+	<div id="page-separator"></div>
+
+			<div id="content" class="subpage">
+				
+                <div id="text">
+                	<h2>Under Maintenance</h2>
+We're sorry, LifeDesks are currently undergoing routine maintenance. We will be back online soon.
+                </div>
+				<img src="images/bluebox_bottom_bg.gif" alt="" />
+				
+			</div><!-- /content -->
+			
+			<div id="sidebar">
+			</div><!-- /sidebar -->
+
+			<div class="clear"></div>
+			<div id="footer"></div>
+			</div><!-- /container -->
+
+		</div><!-- /wrapper -->
+
+</body>
+</html>
+
